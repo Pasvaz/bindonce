00e8b78554eae159c269aa21976716c3ad1e0463
Dean Poulin <dpoulin@healthmarketscience.com>
2015-01-30 14:50:35 -0500

    Updated App Structure and Build for Easier Development Workflow - Added bower dependency on Angular (Should this be 1.x vs 1.2.x?) - Added jshint - Fixed jshint errors in code   line 1    col 13  Strings must use singlequote.   line 18   col 35  Strings must use singlequote.   line 36   col 19  Strings must use singlequote.   line 140  col 44  Expected '!==' and instead saw '!='.   line 158  col 20  Don't make functions within a loop.   line 167  col 26  'ctrl' is already defined.   line 204  col 18  Don't make functions within a loop.   line 232  col 28  Strings must use singlequote.   line 292  col 19  Bad line breaking before '&&'.   line 301  col 59  Strings must use singlequote. - Added gulp build with uglify, ngAnnotate, etc... - Added .gitignore - New development workflow: 1) npm install 2) make development changes 3) gulp build 4) tag repository

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var saveLicense = require('uglify-save-license');
+var wrap = require("gulp-wrap");
+var replace = require("gulp-replace");
+var ngAnnotate = require('gulp-ng-annotate');
+
+gulp.task('scripts', function () {
+  return gulp.src(['src/*.js'])
+    .pipe(replace(/'use strict';/g, ''))
+    .pipe(replace(/"use strict";/g, ''))
+    .pipe(ngAnnotate())
+    .pipe(wrap("(function(){\n'use strict';\n<%= contents %>\n})();"))
+    .pipe($.concat('bindonce.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe($.uglify({preserveComments: saveLicense}))
+    .pipe($.rename('bindonce.min.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe($.size());
+});
+
+gulp.task('jshint', function () {
+  return gulp.src(['src/*.js'])
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.size());
+});
+
+gulp.task('clean', function () {
+  return gulp.src(['.tmp', 'dist'], { read: false })
+    .pipe($.rimraf());
+});
+
+gulp.task('build', ['jshint', 'scripts']);
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
